49f44481606fdb8336a0186347e539fb76c7f1bd
nisreen <nisokh7@gmail.com>
2023-08-23 15:11:52 +0200

    commit add le camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+
+
 import * as L from 'leaflet';
+
+import { Geolocation } from '@capacitor/geolocation'; // V6 >
+
+import { Capacitor } from '@capacitor/core';
+
+import { AlertController } from '@ionic/angular';
+
+//import { Plugins } from '@capacitor/core' V5 <
 
 @Component({
   selector: 'app-home',
@@ -8,23 +18,52 @@ import * as L from 'leaflet';
 })
 export class HomePage implements OnInit {
   map?: L.Map
-  constructor() {}
+
+  constructor(private alertCtrl: AlertController) {}
 
   ionViewWillEnter(){
-    this.map = L.map('mapId').setView([45.2026127,5.7632468], 13);
+    this.locateUser()
+  }
+
+  showMap(lat: number, lng: number){
+    this.map = L.map('mapId').setView([lat,lng], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    L.marker([45.2026127,5.7632468],
+    L.marker(
+      [lat,lng],
       {icon: L.icon({iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png'})})
-    .addTo(this.map)
-
-    .bindPopup('A pretty place in Carrefour')
-
+      .addTo(this.map)
+    .bindPopup('Home sweet Home')
     .openPopup();
   }
-  ngOnInit(): void {
 
+  ngOnInit(): void {
   }
+
+  private showErrorAlert(){
+    this.alertCtrl
+    .create({
+      header: 'Could not fetch location',
+      message:'Please use the map to pick a location'})
+    .then(alertEl => alertEl.present())
+  }
+
+  private locateUser(){
+    if(!Capacitor.isPluginAvailable('Geolocation')){
+      this.showErrorAlert()
+      return;
+    }
+    Geolocation.getCurrentPosition()
+    .then(geoPosition => {
+      const coordinates = {
+        latitude: geoPosition.coords.latitude,
+        longitude: geoPosition.coords.longitude}
+        this.showMap(coordinates.latitude, coordinates.longitude)
+    })
+    .catch(err => this.showErrorAlert())
+  }
+
+
 }
